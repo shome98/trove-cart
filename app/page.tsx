e70aba437fe56259cf0e647922230601771beaad
Sowparna Shome <soupornoshome@gmail.com>
2025-11-16 22:26:44 +0530

    using new styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl text-center">hello from next</h1>
+      <h1 className="text-heading-1">hello from next</h1>
     </>
   );
 }
